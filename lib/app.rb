b8d8a549ae67a1ec0e9f4e8f1d3264aa0d31c13e
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-21 16:48:50 +0000

    added params for the named cat and included if statment to only print welcome message if name is supplied

--- lib/app.rb
+++ lib/app.rb
@@ -9,7 +9,13 @@ get '/secret' do
   "This page is secret..shhhh"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = %w( Pepsi Cola Fanta).sample
-  erb(:index) 
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
+  erb(:index)
 end
